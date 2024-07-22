8511f142e48a06f6dba12f5b6bfc4b90b11edf88
Om Gore <omgore4307@gmail.com>
2024-07-22 19:01:13 +0530

    FINAL

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/shoppingreact",
   plugins: [react()],
+  build: {
+    outDir: 'build' // Ensure this path is correct and does not have any trailing semicolons or typos
+  }
 })
